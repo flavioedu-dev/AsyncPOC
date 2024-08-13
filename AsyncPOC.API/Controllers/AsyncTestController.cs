d0e9bb3edbfb6b4c12bd2aba351d048ac4569194
Flávio Eduardo <xflax00@gmail.com>
2024-08-12 22:40:02 -0300

    feat: Prova de conceito do funcionamento de assincronismo com o uso do await.

---
+++ AsyncPOC.API/Controllers/AsyncTestController.cs
@@ -0,0 +1,19 @@
+﻿using AsyncPOC.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncPOC.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AsyncTestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        AsyncMethods asyncTest = new AsyncMethods();
+
+        await asyncTest.ExecMethods();
+
+        return StatusCode(StatusCodes.Status200OK, null);
+    }
+}
---
+++ AsyncPOC.Application/AsyncMethods.cs
@@ -0,0 +1,29 @@
+﻿namespace AsyncPOC.Application;
+
+public class AsyncMethods
+{
+    public async Task ExecMethods()
+    {
+        AnoPar();
+
+        ContandoLentamente();
+    }
+
+    private async Task<bool> AnoPar()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        bool res = DateTime.Now.Year % 2 == 0 ? true : false;
+        Console.WriteLine("O ano é par? {0}", res ? "sim" : "não");
+        return await Task.FromResult(res);
+    }
+
+    private async Task ContandoLentamente()
+    {
+        int cont = 0;
+        while (++cont <= 10)
+        {
+            Console.WriteLine("Cont: {0}", cont);
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
